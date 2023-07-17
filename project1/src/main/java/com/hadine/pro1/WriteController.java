338de216c75948fd25c7694d37734272b02ca8a0
lg <lg@DESKTOP-ECKBUC6>
2023-07-18 00:43:28 +0900

    update

--- project1/src/main/java/com/hadine/pro1/WriteController.java
+++ project1/src/main/java/com/hadine/pro1/WriteController.java
@@ -1,7 +1,12 @@
 package com.hadine.pro1;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WriteController {
@@ -10,4 +15,13 @@ public class WriteController {
 	public String write() {
 		return "write";
 	}
+	
+	@PostMapping("/write")
+	public String write2(HttpServletRequest request) throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		System.out.println(request.getParameter("title"));
+		System.out.println(request.getParameter("content"));
+		return "board";
+	}
+	
 }
